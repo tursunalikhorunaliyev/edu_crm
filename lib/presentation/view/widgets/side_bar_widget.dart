ca801189ddbf7e95d8873c934c2d6a2dfc4eafd2
RahmatullohXolmatov <rholmatov151@gmail.com>
2023-06-27 17:18:14 +0500

    salomsalomsalomsalomsalsom

--- lib/presentation/view/widgets/side_bar_widget.dart
+++ lib/presentation/view/widgets/side_bar_widget.dart
@@ -1,16 +1,45 @@
+import 'package:edu_crm/presentation/view/widgets/text_plus_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SideBarWidget extends StatelessWidget {
   const SideBarWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 40),
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
         child: Column(
-          children: [],
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 28,
+                  height: 28,
+                  child: Image.asset(
+                    "icon1.png",
+                    width: 28,
+                    height: 28,
+                  ),
+                ),
+                const SizedBox(width: 5),
+                Text(
+                  "REduse",
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 32,
+                  ),
+                ),
+              ],
+            ),
+            const TextPlusWidget(
+              text: "PAGES",
+              iconPath: "Plus.png",
+            )
+          ],
         ),
       ),
     );
--- lib/presentation/view/widgets/text_plus_widget.dart
+++ lib/presentation/view/widgets/text_plus_widget.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/widgets.dart';
 
 class TextPlusWidget extends StatelessWidget {
-  const TextPlusWidget({super.key});
+  final String text;
+  final String iconPath;
+  const TextPlusWidget({super.key, required this.text, required this.iconPath});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
-          "",
-          style: TextStyle(),
-        )
+          text,
+          style: const TextStyle(
+            color: Color(0xFF7B8190),
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        Image.asset(
+          iconPath,
+          width: 20.5,
+          height: 20.5,
+        ),
       ],
     );
   }
