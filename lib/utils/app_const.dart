244427c8fe994d2b0535a9b93c74cd199c211411
usmonaliyevm <uzayyub2007@gmail.com>
2023-07-04 11:43:40 +0500

    appppppp

--- lib/utils/app_const.dart
+++ lib/utils/app_const.dart
@@ -9,10 +9,20 @@ List<String> eCommorceTexts = [
 List<String> fanVaYonalishlar = [];
 List<String> bosh = [];
 
-Color cHavoRang = const Color(0xFFE0E7FF);
-Color cOchHavoRang = const Color(0xFFE0F2FE);
-Color cYashil = const Color(0xFFE0F2FE);
-Color cToqSariq = const Color(0xFFE0F2FE);
-Color cOchQizil = const Color(0xFFFFE4E6);
-Color cOchKok = const Color(0xFFDBEAFE);
-Color cOtoqKok = const Color(0xFF334155);
+Color color1 = const Color(0xFFE0E7FF);
+Color color2 = const Color(0xFFE0F2FE);
+Color color3 = const Color(0xFFE0F2FE);
+Color color4 = const Color(0xFFE0F2FE);
+Color color5 = const Color(0xFFFFE4E6);
+Color color6 = const Color(0xFFDBEAFE);
+Color color7 = const Color(0xFF334155);
+Color color8 = const Color(0xFFF1F5F9);
+
+Color textcolor1 = const Color(0xFF5D5FEF);
+Color textcolor2 = const Color(0xFF178DCC);
+Color textcolor3 = const Color(0xFF059691);
+Color textcolor4 = const Color(0xFF5D5FEF);
+Color textcolor5 = const Color(0xFF5D5FEF);
+Color textcolor6 = const Color(0xFF5D5FEF);
+Color textcolor7 = const Color(0xFF5D5FEF);
+Color textcolor8 = const Color(0xFF5D5FEF);
