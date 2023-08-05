9ed0daf5a18b31e7e5b842329aecb1934d8228be
usmonaliyevm <uzayyub2007@gmail.com>
2023-08-05 19:10:54 +0500

    sub_subjectdan api orqali malumot keldi

--- lib/presentation/view/parts/new_sub_subject_part.dart
+++ lib/presentation/view/parts/new_sub_subject_part.dart
@@ -1,8 +1,11 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:edu_badge_textfield/edu_badge_textfield.dart';
 import 'package:edu_button/edu_button.dart';
 import 'package:edu_crm/presentation/view/widgets/edu_datepicker_field.dart';
 import 'package:edu_crm/utils/app_const.dart';
 import 'package:edu_drop_down/edu_drop_down.dart';
+import 'package:edu_search_table_drop_down/controllers/models/search_model.dart';
+import 'package:edu_search_table_drop_down/edu_search_table_drop_down.dart';
 import 'package:flutter/material.dart';
 
 class NewSubSubjectPart extends StatelessWidget {
@@ -10,117 +13,173 @@ class NewSubSubjectPart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<SearchModel> list = [
+      SearchModel(id: 1, name: "name"),
+      SearchModel(id: 2, name: "mane"),
+      SearchModel(id: 3, name: "patd"),
+    ];
     return Scaffold(
-      body: Container(
-        width: 800 / 1.5,
-        height: 1000 / 1.5,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  EduButton(
-                    verticalPadding: 6,
-                    horizontalPadding: 20,
-                    text: "Saqlash",
-                    containerColor: Color(0xFF5D5FEF),
+      body: Center(
+        child: Container(
+          width: 850 / 1.5,
+          height: 1000 / 1.5,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    EduButton(
+                      verticalPadding: 6,
+                      horizontalPadding: 20,
+                      text: "Saqlash",
+                      containerColor: Color(0xFF5D5FEF),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const EduBadgeText(
+                          text: "Fan",
+                          textStyle: TextStyle(
+                            color: Color(0xFFA6A6A6),
+                            fontFamily: "Inter",
+                            fontWeight: medium,
+                            fontSize: 15,
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        EduSearchTableDropDown(
+                          searchModelList: list,
+                          width: 250,
+                          dropDownHeight: 200,
+                        ),
+                        const SizedBox(height: 30),
+                        const EduBadgeText(
+                          text: "Ochilgan sana",
+                          textStyle: TextStyle(
+                            color: Color(0xFFA6A6A6),
+                            fontFamily: "Inter",
+                            fontWeight: medium,
+                            fontSize: 15,
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        const EduDataPickerField(
+                          text: "08.07.2023",
+                          width: 250,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const EduBadgeText(
+                          text: "Yo'nalish",
+                          textStyle: TextStyle(
+                            color: Color(0xFFA6A6A6),
+                            fontFamily: "Inter",
+                            fontWeight: medium,
+                            fontSize: 15,
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        EduSearchTableDropDown(
+                          searchModelList: list,
+                          width: 250,
+                          dropDownHeight: 200,
+                        ),
+                        const SizedBox(height: 30),
+                        const EduBadgeText(
+                          text: "Yo'nalish",
+                          textStyle: TextStyle(
+                            color: Color(0xFFA6A6A6),
+                            fontFamily: "Inter",
+                            fontWeight: medium,
+                            fontSize: 15,
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        const EduDataPickerField(
+                          text: "08.07.2023",
+                          width: 250,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(height: 40 / 1.5),
+                const EduBadgeText(
+                  text: "Qizqacha ma'lumot",
+                  textStyle: TextStyle(
+                    color: Color(0xFFA6A6A6),
+                    fontFamily: "Inter",
+                    fontWeight: medium,
+                    fontSize: 15,
                   ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      EduBadgeText(
-                        text: "Fan",
-                        textStyle: TextStyle(
-                          color: Color(0xFFA6A6A6),
-                          fontFamily: "Inter",
-                          fontWeight: medium,
-                          fontSize: 15,
-                        ),
-                      ),
-                      SizedBox(height: 6),
-                      EduDropDown(
-                        width: 230,
-                        elements: [],
-                        contentGap: 6,
-                        isDate: true,
-                        dateList: [
-                          "Malumotni tanlang",
-                          "Oliy",
-                          "O'rta maxsus",
-                          "Bakalavr",
-                        ],
-                      ),
-                      SizedBox(height: 30),
-                      EduBadgeText(
-                        text: "Ochilgan sana",
-                        textStyle: TextStyle(
-                          color: Color(0xFFA6A6A6),
-                          fontFamily: "Inter",
-                          fontWeight: medium,
-                          fontSize: 15,
-                        ),
-                      ),
-                      SizedBox(height: 6),
-                      EduDataPickerField(text: "08.07.2023", width: 230),
-                    ],
+                ),
+                SizedBox(height: 6),
+                Container(
+                  height: 144 / 1.5,
+                  width: 800,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    border: Border.all(
+                      width: 1,
+                      color: const Color(0xFFE2E8F0),
+                    ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      EduBadgeText(
-                        text: "Yo'nalish",
-                        textStyle: TextStyle(
-                          color: Color(0xFFA6A6A6),
-                          fontFamily: "Inter",
-                          fontWeight: medium,
-                          fontSize: 15,
+                ),
+                const SizedBox(height: 40 / 1.5),
+                DottedBorder(
+                  color: const Color(0xFF5D5FEF),
+                  //color of dotted/dash line
+                  strokeWidth: 1,
+                  //thickness of dash/dots
+                  dashPattern: [10, 6],
+                  radius: const Radius.circular(6),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 340 / 1.5,
+                    width: 800,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 200,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        border: Border.all(
+                          width: 1,
+                          color: const Color(0xFFCCD3DB),
                         ),
                       ),
-                      SizedBox(height: 6),
-                      EduDropDown(
-                        width: 230,
-                        elements: [],
-                        contentGap: 6,
-                        isDate: true,
-                        dateList: [
-                          "Malumotni tanlang",
-                          "Oliy",
-                          "O'rta maxsus",
-                          "Bakalavr",
-                        ],
-                      ),
-                      SizedBox(height: 30),
-                      EduBadgeText(
-                        text: "Yo'nalish",
-                        textStyle: TextStyle(
-                          color: Color(0xFFA6A6A6),
+                      child: const Text(
+                        "Rasmni tanlang",
+                        style: TextStyle(
+                          color: Color(0xFF726BEA),
+                          fontSize: 18,
                           fontFamily: "Inter",
                           fontWeight: medium,
-                          fontSize: 15,
                         ),
                       ),
-                      SizedBox(height: 6),
-                      EduDataPickerField(
-                        text: "08.07.2023",
-                        width: 230,
-                      ),
-                    ],
-                  )
-                ],
-              )
-            ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
